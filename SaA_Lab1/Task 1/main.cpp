d3540702ef0b99571700673517a1e23e5ffaca55
PaNik2000 <arshakov-nikita@mail.ru>
2020-02-16 18:19:54 +0300

    Added main loop

--- SaA_Lab1/Task 1/main.cpp
+++ SaA_Lab1/Task 1/main.cpp
@@ -1,18 +1,50 @@
 #include "tree.h"
 #include <iostream>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
 
 int main(int argc, char** argv)
 {
-    Tree tree = Tree("Oleg", 2);
+    string name;
+    int count;
 
-    tree.addSon("Oleg", "Denis", 3);
-    tree.addSon("Denis", "Nikita", 1);
-    tree.addSon("Nikita", "Artem", 0);
-    tree.addSon("Denis", "Leisan", 0);
-    tree.addSon("Denis", "Aleksey", 0);
-    tree.addSon("Denis", "Ivan", 3);
+    cout << "Enter the name of root person and number of his descendants: ";
+    cin >> name >> count;
+
+    Tree tree = Tree(name, count);
+
+    bool enterStop = false;
+    while (!enterStop)
+    {   char choice;
+        cout << "Do you want to enter more persons? (y/n)\nAnswer: ";
+        cin >> choice;
+        if (choice == 'n' || choice == 'N')
+        {
+            enterStop = true;
+        }
+        else if (choice == 'y' || choice == 'Y')
+        {
+            string ancestor;
+            cout << "Enter ancestor's name, name of the person and number of descendants: ";
+            cin >> ancestor >> name >> count;
+
+            if (tree.find(name) != nullptr)
+            {
+                cout << "Error : Person with name " << name << " already exists" << endl;
+            }
+            else
+            {
+                tree.addSon(ancestor, name, count);
+            }
+        }
+        else
+        {
+            cout << "Error : Incorrect choice" << endl;
+        }
+    }
 
     return 0;
 }
